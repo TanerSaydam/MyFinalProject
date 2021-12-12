dccbb8188f9116e564615b7305ddafb3f4f6b525
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-13 02:18:22 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstract;
+using Core.Unilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IResult Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Category> GetById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Category>> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product();
+            product.ProductId = 6;
+            product.ProductName = "Deneme";
+            product.CategoryId = 2;
+            product.UnitPrice = 100;
+            product.UnitsInStock = 2;
+
+            Product product2 = new Product();
+            product2.ProductId = 7;
+            product2.ProductName = "Deneme2";
+            product2.CategoryId = 22;
+            product2.UnitPrice = 100;
+            product2.UnitsInStock = 2;
+
+            InMemoryProductDal inMemoryProductDal = new InMemoryProductDal();
+            inMemoryProductDal.Add(product);
+            inMemoryProductDal.Add(product2);
+
+            foreach (var item in inMemoryProductDal.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            inMemoryProductDal.Delete(product2);
+
+            foreach (var item in inMemoryProductDal.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            product.ProductName = "Bilgisayar";
+            inMemoryProductDal.Update(product);
+
+            foreach (var item in inMemoryProductDal.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
